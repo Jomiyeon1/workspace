1a018d95604a4372a8e49b1b5c27681f1ee329c7
MY <fourp@naver.com>
2022-03-14 23:25:38 +0900

    2022.03.14 23:25

--- HomeWork/src/March/miyeon08/EmpStatistics.java
+++ HomeWork/src/March/miyeon08/EmpStatistics.java
@@ -2,7 +2,9 @@ package March.miyeon08;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class EmpStatistics {
@@ -20,15 +22,30 @@ public class EmpStatistics {
 	//method
 	public void printStatistice(int maxSalary) throws SQLException{
 		
-		
 		System.out.println("==============================");
 		System.out.println("MaxSalary : " + maxSalary);
 		System.out.println("==============================");
 		
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
 		Connection con = DriverManager.getConnection(url,"hr","hr");
+		Statement stmt = con.createStatement();
+		String sql = "SELECT j.job_title, avg(e.salary)\r"
+				+ "from employees e, jobs j\r"
+				+ "where e.job_id = j.job_id\r"
+				+ "GROUP BY j.job_title\r"
+				+ "HAVING avg(e.salary) >= \r" + maxSalary
+				+ "ORDER BY avg(e.salary) desc";
+		
+		ResultSet rs = stmt.executeQuery(sql);
 		
 		
-		
+		while(rs.next()) {
+			System.out.println("[" + rs.getString("job_title") + "]" + rs.getInt("avg(e.salary)") );
+			
+		}
+		rs.close();
+		stmt.close();
+		con.close();
 		
 	}//end of method
 	
