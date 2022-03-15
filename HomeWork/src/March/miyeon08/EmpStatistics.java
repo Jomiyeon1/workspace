ca762025c3c7e6e9dfd4ef29bdf894f8493d2d56
MY <fourp@naver.com>
2022-03-15 23:33:11 +0900

    2022.03.15 23:33

--- HomeWork/src/March/miyeon08/EmpStatistics.java
+++ HomeWork/src/March/miyeon08/EmpStatistics.java
@@ -8,8 +8,6 @@ import java.sql.Statement;
 
 
 public class EmpStatistics {
-	String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-	String driver = "oracle.jdbc.driver.OracleDriver";
 	
 	static {
 		try {
