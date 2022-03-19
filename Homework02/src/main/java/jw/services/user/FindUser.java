a7068f1c9f774810461568c9538fd26e19972fe6
MY <fourp@naver.com>
2022-03-19 22:42:08 +0900

    2022.03.19 22:42

--- Homework02/src/main/java/jw/services/user/FindUser.java
+++ Homework02/src/main/java/jw/services/user/FindUser.java
@@ -5,9 +5,11 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import jw.services.user.dao.UserDAO;
 import jw.services.user.vo.UserVO;
@@ -15,47 +17,76 @@ import jw.services.user.vo.UserVO;
 //@WebServlet("/FindUser")
 public class FindUser extends HttpServlet {
 
-	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
 		req.setCharacterEncoding("EUC_KR");
 		res.setContentType("text/html;charset=EUC_KR");
 		PrintWriter out = res.getWriter();
+		//Session 생성===========================================
+		HttpSession session = req.getSession(true);
+		System.out.println("==============Session 생성 완료");
 
 		// client에서 넘어온 값을 받자.
 		String name = req.getParameter("name");
-//		String gender = req.getParameter("gender");
-//		String year = req.getParameter("year");
-//		String month = req.getParameter("month");
-//		String day = req.getParameter("day");
-//		String num = req.getParameter("num");
-//		String addr = req.getParameter("addr");
+
 		UserDAO bean = new UserDAO();
 		bean.findUser(name);
 
-		// Client에 받은 data로 UserVO instance 생성 및 Value set
-//		UserVO userVO = new UserVO();
-//		userVO.setName(name);
-//		userVO.setGender(gender);
-//		userVO.setYear(year);
-//		userVO.setMonth(month);
-//		userVO.setDay(day);
-//		userVO.setNum(num);
-//		userVO.setAddr(addr);
+		//Session 저장하기
+	
+//		Cookie[] cookies = req.getCookies();
+//		if (cookies != null) {
+//			for(int i = 0; i < cookies.length; i++)
+//			System.out.println("\n cookie에 저장된 정보 :");
+//			System.out.println(cookies[i].getName() + " : " + cookies[i].getValue());
+//			
+//		}
 
+		//session이 있다면? 값을 불러오자
+		
+		
 		// DB에 접근하는 UserDAO를 이용 회원정보 select
 		UserVO userVO = new UserVO();
 		userVO = bean.findUser(name);
-
+//		if(session != null) {
+//		userVO = (UserVO) session.getAttribute("UserVO");
+//		}
+		if(session.isNew()) {
+			session.setAttribute("FindUser", userVO);
+			System.out.println("============session 저장완료");
+			
+			
+		}
+		
 		out.println("<html>");
 		out.println("<body>");
 
 		out.println("<h2>회원정보</h2>");
-
+		
+		
+	//	if( userVO.isActive()){
+			
+		
 		out.println("이름 : " + userVO.getName());
 		out.println("성별 : " + userVO.getGender());
 		out.println("생년월일 : " + userVO.getYear() + "/" + userVO.getMonth() + "/" + userVO.getDay() );
-		out.println("전화번호 : " + userVO.getNum());
+		out.println("전화번호 : " + userVO.getNum() );
 		out.println("주소 : " + userVO.getAddr());
+		out.println("JSESSIONID = " + session.getId());
+
+		
+				
+	//	}else {
+			
+	//		out.println("회원아닙니다.");
+	//	}
+		// 확인하기
+		
+
+		
+		
+		
+		
 
 		out.println("</body>");
 		out.println("</html>");
